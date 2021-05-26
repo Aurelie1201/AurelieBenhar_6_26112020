2a3602dd2ef8a5cf20e51cec0da96e8c60f09c63
Aurelie Benhar <aurelie.120183@hotmail.fr>
2021-05-26 15:29:31 +0200

    Correction pour l'affichage des images

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path');
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauces');
@@ -19,6 +20,8 @@ app.use((req, res, next) =>{
 });
 
 app.use(express.json());//Remplace bodyParser.json()
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', sauceRoutes);
